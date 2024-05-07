cb0734566d011c1a9c47324bf0f6d20089853ada
JahanZaibAsif <jahanzaibasif969@gmail.com>
2024-05-07 11:10:02 +0500

    Change in Route

--- controller/booking.js
+++ controller/booking.js
@@ -1,9 +1,7 @@
 // backend
-// const Listing = require('../model/listing');
-// const User = require('../model/user');
 const Booking = require('../model/booking');
 const moment = require('moment');
-const stripe = require('stripe')(process.env.stripe_screat); // Replace with your test secret key
+const stripe = require('stripe')(process.env.stripe_screat);
 
 const create_booking = async (req, res) => {
     const userId = '65ce14ac157012fc545780eb';
@@ -11,8 +9,8 @@ const create_booking = async (req, res) => {
     const paymentMethodId = req.body.paymentMethodId;
     const total_amount = req.body.totalAmount; 
 
-    const start_date = moment(req.body.startDate, 'DD-MM-YYYY').toDate(); // corrected property name
-    const end_date = moment(req.body.endDate, 'DD-MM-YYYY').toDate(); // corrected property name
+    const start_date = moment(req.body.startDate, 'DD-MM-YYYY').toDate(); 
+    const end_date = moment(req.body.endDate, 'DD-MM-YYYY').toDate();
     const { adults, children, infants, pets } = req.body;
 
     try {
@@ -45,11 +43,9 @@ const create_booking = async (req, res) => {
                 });
             }
         } else {
-            // Handle payment failure
             res.status(400).json({ message: 'Payment failed' });
         }
     } catch (error) {
-        // Handle any other errors
         console.error('Error creating booking:', error);
         res.status(500).json({ error: 'An error occurred while processing your request.' });
     }
--- routes/airbnb.js
+++ routes/airbnb.js
@@ -30,11 +30,10 @@ router.post('/emailverification', upload.none(), emailController.confirmEmail);
 router.post('/verificationCode', upload.none(), SinUpController.verificationCode);
 router.post('/userData', checkRoute, SinUpController.userData);
 router.post('/create_listing', listingController.upload_picture, checkRoute, listingController.create_listing);
-router.get('/show_listing', checkRoute, SinUpController.showAll_listing);
-router.delete('/:id/delete_listing/', checkRoute, SinUpController.delete_listing);
-router.put('/:id/update_listing', checkRoute, listingController.upload_picture, listingController.update_listing);
-router.post('/newApi', SinUpController.newApi);
-router.get('/:id/single_listing', checkRoute, listingController.single_listing);
+router.get('/show_listing', checkRoute, listingController.showAll_listing);
+router.delete('/delete_listing/:id', checkRoute, listingController.delete_listing);
+router.put('/update_listing/:id', checkRoute, listingController.upload_picture, listingController.update_listing);
+router.get('/single_listing/:id', checkRoute, listingController.single_listing);
 router.post('/create_booking', upload.none(), bookingController.create_booking);
 router.get('/create_booking', checkRoute, listingController.getCoordinates);
 
